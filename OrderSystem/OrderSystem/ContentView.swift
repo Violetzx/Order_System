24389846c616ea953b30ff89afd6eb361799568b
YunLin Huang <karta035865@gmail.com>
2021-06-27 10:21:04 +0800

    1
    beginning version

---
+++ OrderSystem/OrderSystem/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OrderSystem
+//
+//  Created by YunLinHuang on 2021/6/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OrderSystem/OrderSystem/OrderSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderSystemApp.swift
+//  OrderSystem
+//
+//  Created by YunLinHuang on 2021/6/27.
+//
+
+import SwiftUI
+
+@main
+struct OrderSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
